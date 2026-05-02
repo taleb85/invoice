575f72eb690cf37470994144f82cc241f07913ef
Taleb Barikhan <talebbarikhan@gmail.com>
2026-05-02 13:52:29 +0100

    chore: auto-commit agent changes (2026-05-02 13:52)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -631,7 +631,7 @@ export default function Sidebar({ onClose }: SidebarProps) {
                           d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                         />
                       </svg>
-                      <span className="min-w-0 leading-snug">{t.nav.sedeGlobalOverview}</span>
+                      <span className="min-w-0 leading-snug">{t.sedi.adminRole}</span>
                     </button>
                   ) : null}
                 </div>
